d2939511a428a732cca9f9da37c58bb023836b8e
jsantos93 <jcas@cesar.school>
2020-03-25 20:37:14 -0300

    update graph class

--- Graph.java
+++ Graph.java
@@ -2,29 +2,6 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Graph {
-    public class Node {
-        String nome;
-        List<Edge> adj;
-
-        Node(String nome) {
-            this.nome = nome;
-            this.adj = new ArrayList<Edge>();
-        }
-
-        void addAdj(Edge e) {
-            adj.add(e);
-        }
-    }
-
-    public class Edge {
-        Node origem;
-        Node destino;
-
-        Edge(Node origem, Node destino) {
-            this.origem = origem;
-            this.destino = destino;
-        }
-    }
 
     List<Node> vertices;
     List<Edge> arestas;
@@ -34,8 +11,41 @@ public class Graph {
         arestas = new ArrayList<Edge>();
     }
 
+    Node addNode(String nome) {
+        Node v = new Node(nome);
+        vertices.add(v);
+        return v;
+    }
+
+    Edge addEdge(Node origem, Node destino) {
+        Edge e = new Edge(origem, destino);
+        origem.addAdj(e);
+        arestas.add(e);
+        return e;
+    }
+
+    public String toString() {
+        String r = "";
+        for (Node u : vertices) {
+            r += u.nome + " -> ";
+            for (Edge e : u.adj) {
+                Node v = e.destino;
+                r += v.nome + ", ";
+            }
+            r += "\n";
+        }
+        return r;
+    }
+
     public static void main(String[] args) {
         Graph g = new Graph();
+        Node s = g.addNode("s");
+        Node t = g.addNode("t");
+        Node y = g.addNode("y");
+        Edge st = g.addEdge(s, t);
+        Edge sy = g.addEdge(s, y);
+        Edge ty = g.addEdge(t, y);
+        Edge yt = g.addEdge(y, t);
         System.out.println(g);
     }
 }
